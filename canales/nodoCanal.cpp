2bde3166a74ff2d3d6cf33875748749f6117a978
Jhonny <jcamacho@growthaccelerationpartners.com>
2016-11-21 00:33:48 -0600

    Adding nodes to build the lists

---
+++ canales/nodoCanal.cpp
@@ -0,0 +1,35 @@
+#include "nodoCanal.h"
+
+NodoCanal::NodoCanal(){}
+
+NodoCanal::NodoCanal(Canal dato ){
+	this->dato = dato;
+	siguiente = NULL;
+}
+
+NodoCanal::~NodoCanal(){
+}
+
+void NodoCanal::setSig(NodoCanal *n){
+	siguiente = n;
+}
+
+void NodoCanal::setAnt(NodoCanal *n){
+	anterior = n;
+}
+
+void NodoCanal::setDato(Canal x){
+	dato = x;
+}
+
+int NodoCanal::getDato(){
+	return dato;
+}
+
+NodoCanal* NodoCanal::getSig(){
+	return siguiente;
+}
+
+NodoCanal* NodoCanal::getAnt(){
+	return anterior;
+}
---
+++ canales/nodoCanal.h
@@ -0,0 +1,26 @@
+#ifndef NODOCANAL_H_INCLUDED
+#define NODOCANAL_H_INCLUDED
+#include "canal.h"
+
+class NodoCanal{
+    public:
+        NodoCanal();
+        NodoCanal(Canal x);
+        ~NodoCanal();
+        void setDato(Canal x);
+        Canal getDato();
+        void setSig(NodoCanal *n);
+        void setAnt(NodoCanal *n);
+        NodoCanal* getSig();
+        NodoCanal* getAnt();
+
+
+
+    private:
+        Canal *dato;
+        NodoCanal *siguiente;
+        NodoCanal *anterior;
+};
+
+
+#endif // NODOCANAL_H_INCLUDED
