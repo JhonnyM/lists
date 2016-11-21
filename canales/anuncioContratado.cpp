ad82d32598d543c5e3964ca89fb422f0c02ea736
Jhonny <jcamacho@growthaccelerationpartners.com>
2016-11-21 00:18:43 -0600

    Add anuncio contratado

---
+++ canales/anuncioContratado.cpp
@@ -0,0 +1,9 @@
+#include "anuncioContratado.h"
+
+void AnuncioContratado::setCostoTotal(double costoTotal){
+    this->costoTotal = costoTotal;
+};
+
+double AnuncioContratado::getCostoTotal(){
+    return costoTotal;
+};
---
+++ canales/anuncioContratado.h
@@ -0,0 +1,15 @@
+#ifndef ANUNCIOCONTRATADO_H_INCLUDED
+#define ANUNCIOCONTRATADO_H_INCLUDED
+class AnuncioContratado{
+    private:
+        double costoTotal;
+        // Add a list type here that points to a sublist of tipe Anuncio
+
+    public:
+        void setCostoTotal;
+        double getCostoTotal;
+
+};
+
+
+#endif // ANUNCIOCONTRATADO_H_INCLUDED
